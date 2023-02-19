1fdc72da000aefd17fe7a16193bb48706510e9df
Амир <Smiker1@yandex.ru>
2023-02-19 13:18:39 +0300

    tasks

---
+++ Lesson_5/views.py
@@ -0,0 +1,142 @@
+from framework.templator import render
+from patterns.сreational_patterns import Engine, Logger
+from patterns.structural_patterns import AppRoute, Debug
+
+site = Engine()
+logger = Logger('main')
+
+routes = {}
+
+
+@AppRoute(routes=routes, url='/')
+class Index:
+    @Debug(name='Index')
+    def __call__(self, request):
+        return '200 OK', render('index.html', date=request.get('date', None))
+
+
+@AppRoute(routes=routes, url='/about/')
+class About:
+    @Debug(name='About')
+    def __call__(self, request):
+        return '200 OK', render('about.html', date=request.get('date', None))
+
+
+@AppRoute(routes=routes, url='/contact/')
+class Contact:
+    @Debug(name='Contact')
+    def __call__(self, request):
+        return '200 OK', render('contact.html', date=request.get('date', None))
+
+
+@AppRoute(routes=routes, url='/study_programs/')
+class StudyPrograms:
+    @Debug(name='StudyPrograms')
+    def __call__(self, request):
+        return '200 OK', render('study_programs.html', date=request.get('date', None), objects_list=site.categories)
+
+
+class NotFound404:
+    @Debug(name='NotFound404')
+    def __call__(self, request):
+        return '404 WHAT', '404 PAGE Not Found'
+
+
+@AppRoute(routes=routes, url='/courses_list/')
+class CoursesList:
+    def __call__(self, request):
+        logger.log('Список курсов')
+        try:
+            category = site.find_category_by_id(
+                int(request['request_params']['id']))
+            return '200 OK', render('course_list.html',
+                                    objects_list=category.courses,
+                                    name=category.name, id=category.id)
+        except KeyError:
+            return '200 OK', 'No courses have been added yet'
+
+
+@AppRoute(routes=routes, url='/create_course/')
+class CreateCourse:
+    category_id = -1
+
+    def __call__(self, request):
+        if request['method'] == 'POST':
+            data = request['data']
+            name = data['name']
+            name = site.decode_value(name)
+
+            category = None
+            if self.category_id != -1:
+                category = site.find_category_by_id(int(self.category_id))
+
+                course = site.create_course('record', name, category)
+                site.courses.append(course)
+
+            return '200 OK', render('course_list.html',
+                                    objects_list=category.courses,
+                                    name=category.name,
+                                    id=category.id)
+
+        else:
+            try:
+                self.category_id = int(request['request_params']['id'])
+                category = site.find_category_by_id(int(self.category_id))
+
+                return '200 OK', render('create_course.html',
+                                        name=category.name,
+                                        id=category.id)
+            except KeyError:
+                return '200 OK', 'No categories have been added yet'
+
+
+@AppRoute(routes=routes, url='/create_category/')
+class CreateCategory:
+    def __call__(self, request):
+
+        if request['method'] == 'POST':
+            data = request['data']
+            name = data['name']
+            name = site.decode_value(name)
+            category_id = data.get('category_id')
+
+            category = None
+            if category_id:
+                category = site.find_category_by_id(int(category_id))
+
+            new_category = site.create_category(name, category)
+            site.categories.append(new_category)
+            return '200 OK', render('index.html', objects_list=site.categories)
+        else:
+            categories = site.categories
+            return '200 OK', render('create_category.html',
+                                    categories=categories)
+
+
+@AppRoute(routes=routes, url='/category_list/')
+class CategoryList:
+    def __call__(self, request):
+        logger.log('Список категорий')
+        return '200 OK', render('category_list.html',
+                                objects_list=site.categories)
+
+
+@AppRoute(routes=routes, url='/copy_course/')
+class CopyCourse:
+    def __call__(self, request):
+        request_params = request['request_params']
+
+        try:
+            name = request_params['name']
+            old_course = site.get_course(name)
+            if old_course:
+                new_name = f'copy_{name}'
+                new_course = old_course.clone()
+                new_course.name = new_name
+                site.courses.append(new_course)
+
+            return '200 OK', render('course_list.html',
+                                    objects_list=site.courses,
+                                    name=new_course.category.name)
+        except KeyError:
+            return '200 OK', 'No courses have been added yet'
